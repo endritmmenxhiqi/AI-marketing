5d9b4371564b1b0151ce070e2a2afefd06db7291
fnebihi10 <nebihifatlind@gmail.com>
2026-04-23 21:15:33 +0200

    chore(backend): remove legacy javascript sources

--- backend/src/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const authController = require('./authController');
-const aiController = require('./aiController');
-const generationController = require('./generationController');
-
-module.exports = {
-  ...authController,
-  ...aiController,
-  ...generationController,
-};
--- backend/src/models/index.js
+++
@@ -1,7 +0,0 @@
-const User = require('./User');
-const Generation = require('./Generation');
-
-module.exports = {
-  User,
-  Generation,
-};
